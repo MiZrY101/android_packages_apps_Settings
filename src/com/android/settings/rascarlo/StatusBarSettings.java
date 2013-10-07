064cbe9cc22472a406cf6bff5377997bfa2338cc
rusty <slayjay78@gmail.com>
2013-10-07 14:00:43 -0700

    add statusbar clock hide and am/pm

--- src/com/android/settings/rascarlo/StatusBarSettings.java
+++ src/com/android/settings/rascarlo/StatusBarSettings.java
@@ -18,10 +18,15 @@ import com.android.settings.Utils;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
 OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_CLOCK_CATEGORY = "status_bar_clock_category";
+    private static final String STATUS_BAR_CLOCK = "status_bar_show_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm"; 
     private static final String QUICK_PULLDOWN = "quick_pulldown";
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     private static final String STATUS_BAR_BATTERY = "status_bar_battery";
 
+    private ListPreference mStatusBarAmPm;
+    private CheckBoxPreference mStatusBarClock; 
     private ListPreference mQuickPulldown;
     private CheckBoxPreference mStatusBarBrightnessControl;
     private ListPreference mStatusBarBattery;
@@ -32,6 +37,29 @@ OnPreferenceChangeListener {
 
         addPreferencesFromResource(R.xml.status_bar_settings);
 
+        // Clock
+        mStatusBarClock = (CheckBoxPreference) getPreferenceScreen().findPreference(STATUS_BAR_CLOCK);
+        mStatusBarClock.setChecked((Settings.System.getInt(getActivity().
+                getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_CLOCK, 1) == 1));
+
+        // Am-Pm
+        mStatusBarAmPm = (ListPreference) getPreferenceScreen().findPreference(STATUS_BAR_AM_PM);
+        try {
+            if (Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.TIME_12_24) == 24) {
+                mStatusBarAmPm.setEnabled(false);
+                mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+            }
+        } catch (SettingNotFoundException e ) {
+        }
+
+        int statusBarAmPm = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_AM_PM, 2);
+        mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
+        mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
+        mStatusBarAmPm.setOnPreferenceChangeListener(this);
+ 
         // status bar battery
         mStatusBarBattery = (ListPreference) getPreferenceScreen().findPreference(
                 STATUS_BAR_BATTERY);
@@ -44,12 +72,37 @@ OnPreferenceChangeListener {
      }    
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mStatusBarBattery) {
+
+        // Am-Pm
+        if (preference == mStatusBarAmPm) {
+            int statusBarAmPm = Integer.valueOf((String) objValue);
+            int indexAmPm = mStatusBarAmPm.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().
+                    getContentResolver(), Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[indexAmPm]);
+            return true;
+
+        // Battery
+        } else if (preference == mStatusBarBattery) { 
             int statusBarBattery = Integer.valueOf((String) objValue);
             int index = mStatusBarBattery.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.STATUS_BAR_BATTERY, statusBarBattery);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        boolean value;
+        
+        // Clock
+        if (preference == mStatusBarClock) {
+            value = mStatusBarClock.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_CLOCK, value ? 1 : 0);
             return true;
         }
         return false;
